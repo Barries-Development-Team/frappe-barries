fd4f5049cffeb5763204e68e46e9cc743d76c2a0
Barries Ski and Sports <barriesskiandsports@gmail.com>
2026-03-30 16:21:03 -0600

    Added comment to Swap Price script.

--- barries/overrides/item.py
+++ barries/overrides/item.py
@@ -2,6 +2,7 @@
 # This allows the custom_swap_price field to be used in pricing rules and other calculations without needing to reference the child table directly.
 # Fancy way to say we use it for the Swap Tag.
 def validate(doc, method):
+    # Look for the "Swap Price" entry in the custom_pricing child table.
     swap_row = next(
         (row for row in doc.custom_pricing if row.price_list == "Swap Price"),
         None
