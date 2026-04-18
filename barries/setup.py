46be11809323f5fabc88a04e8dfed78d1e966071
Carter <carterluker13@gmail.com>
2026-04-18 00:05:08 +0000

    Moved type override to dedicated file in overrides directory.

--- barries/setup.py
+++ barries/setup.py
@@ -1,26 +1,8 @@
 import frappe
 
+from barries.overrides.item_barcode import override_barcode_type
+
 
 # Override standard barcode types for Item Barcode doctype.
 def set_property_setters():
-	frappe.db.delete(
-		"Property Setter",
-		{
-			"doc_type": "Item Barcode",
-			"field_name": "barcode_type",
-			"property": "options",
-		},
-	)
-	doc = frappe.get_doc(
-		{
-			"doctype": "Property Setter",
-			"doctype_or_field": "DocField",
-			"doc_type": "Item Barcode",
-			"field_name": "barcode_type",
-			"property": "options",
-			"property_type": "Text",
-			"value": "\nUPC\nEAN\nAscend SKU\nSerial Number\nOption",
-		}
-	)
-	doc.insert(ignore_permissions=True)
-	frappe.db.commit()
+	override_barcode_type()
